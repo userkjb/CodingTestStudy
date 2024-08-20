5c74c389e326bcb86cf588ffea6ae3a7229033e2
kjb <kjb_jh@naver.com>
2024-08-20 14:57:51 +0900

    김종범 : 나머지 값에 따라 값은 출력 함.

--- CT/NumberOfCountries_124/FileName.cpp
+++ CT/NumberOfCountries_124/FileName.cpp
@@ -1,2 +1,60 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+string ReStr(int n);
+string solution(int n);
+
+int main()
+{
+	for (size_t i = 1; i <= 3; i++)
+	{
+		cout << solution(i) << endl;
+	}
+
+	return 0;
+}
+
+string solution(int n)
+{
+	string answer = "";
+	int count = 0;
+	
+	int x = n / 3;
+
+	if (count < 3)
+	{
+		
+	}
+	else
+	{
+		//for (size_t i = 0; i < length; i++)
+		//{
+		//	answer += ReStr(n);
+		//	count++;
+		//}
+	}
+
+
+	// answer += ReStr(n); 이게 1번 실행 될지 2번 실행 될지
+
+	return answer;
+}
+
+string ReStr(int n)
+{
+	vector<string> UsValue;
+	UsValue.push_back("4");
+	UsValue.push_back("1");
+	UsValue.push_back("2");
+
+	string Str = "";
+
+	int y = n % 3;
+
+	Str += UsValue[y];
+
+	return Str;
+}
