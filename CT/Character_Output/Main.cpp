00a1c603fa0971977942d03da1e25d16378b79d3
KJB <kjb_jh@naver.com>
2024-10-14 22:26:03 +0900

    문제 - 문자 출력

---
+++ CT/Character_Output/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	std::string message = "Let's go!";
+
+	std::cout << "3\n2\n1" << std::endl;
+	std::cout << message << std::endl;
+
+	return 0;
+}
